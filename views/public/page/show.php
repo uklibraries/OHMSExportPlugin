7d6d269579df005885cbefe1e1718b402fb194dc
libmanuk <eweig@email.uky.edu>
2015-03-20 16:31:26 -0400

    Create show.php

---
+++ views/public/page/show.php
@@ -0,0 +1,28 @@
+<?php echo '<?xml version="1.0"?>'; ?>
+<ROOT>
+<record id="dipid" dt="2015-01-01">
+<date format="yyyy-mm-dd"><![CDATA[<?php echo item('Item Type Metadata', 'Interview Standard Date'); ?>]]></date>
+<date_nonpreferred_format><![CDATA[<?php echo item('Item Type Metadata', 'Interview Date'); ?>]]></date_nonpreferred_format>
+<cms_record_id><![CDATA[<?php echo item('Dublin Core', 'Identifier'); ?>]]></cms_record_id>
+<title><![CDATA[<?php echo item('Item Type Metadata', 'Interview Title'); ?>]]></title>
+<accession><![CDATA[<?php echo item('Item Type Metadata', 'Interview Accession Number'); ?>]]></accession>
+<duration></duration>
+<collection_id><![CDATA[<?php echo item('Item Type Metadata', 'Interview Collection Identifier'); ?>]]></collection_id>
+<collection_name><![CDATA[<?php echo item('Item Type Metadata', 'Interview Collection'); ?>]]></collection_name>
+<series_id><![CDATA[<?php echo item('Item Type Metadata', 'Interview Series Identifier'); ?>]]></series_id>
+<series_name><![CDATA[<?php echo item('Item Type Metadata', 'Interview Series'); ?>]]></series_name>
+<repository><![CDATA[<?php echo item('Dublin Core', 'Publisher'); ?>]]></repository>
+<repository_url />
+<subject></subject>
+<interviewee><![CDATA[<?php echo item('Item Type Metadata', 'Interviewee'); ?>]]></interviewee>
+<interviewer><![CDATA[<?php echo item('Item Type Metadata', 'Interviewer'); ?>]]></interviewer>
+<file_name></file_name>
+<sync></sync>
+<type>interview</type>
+<description><![CDATA[<?php echo item('Item Type Metadata', 'Interview Summary'); ?>]]></description>
+<rel />
+<transcript></transcript>
+<rights><![CDATA[<?php echo item('Item Type Metadata', 'Title'); ?>]]></rights>
+<usage><![CDATA[<?php echo item('Item Type Metadata', 'Title'); ?>]]></usage>
+</record>
+</ROOT>
